4fb33d2c8076bb6f585324a6c54652b822226a0f
pablo <pablo@DESKTOP-GS9VTSM>
2016-11-07 09:51:23 -0600

    DataSet Login - Pablo

---
+++ Hackaton/BLL/ClasslLog.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL.DataSetLoginTableAdapters;
+using System.Data;
+
+namespace BLL
+{
+    class ClasslLog
+    {
+        private UsuarioTableAdapter  _log;
+        private UsuarioTableAdapter LOG
+        {
+            get
+            {
+                if (_log == null)
+                    _log = new UsuarioTableAdapter();
+                return _log;
+            }
+        }
+             public DataTable Inicio(string _user, string _pass)
+        {
+            DataTable resultado = new DataTable();
+            try
+            {
+                resultado = LOG.GetDataLogin(_user, _pass);
+            }
+            catch (Exception)
+            {
+                resultado = null;
+
+            }
+
+            return resultado;
+        }
+    
+    }
+}
